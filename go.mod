00806b6fb24446b6172b770901636d3f37fa0424
Mahiro-T <mahirokomatuo0102@gmail.com>
2024-03-29 15:28:25 +0900

    rm framework

--- go.mod
+++
@@ -1,7 +0,0 @@
-module github.com/kyoto-framework/new
-
-go 1.21.3
-
-require go.kyoto.codes/v3 v3.1.0
-
-require go.kyoto.codes/zen/v3 v3.1.0 // indirect
--- main.go
+++ main.go
@@ -1,36 +1,4 @@
 package main
 
-import (
-	"flag"
-	"fmt"
-	"go.kyoto.codes/v3/rendering"
-	"net/http"
-)
-
-func setup() *http.ServeMux {
-	// Initialize mux
-	mux := http.NewServeMux()
-
-	// Setup assets
-	mux.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
-
-	// Setup pages
-	mux.Handle("/", rendering.Handler(IndexPage))
-
-	// Setup components (htmx)
-
-	// Return mux
-	return mux
-}
-
 func main() {
-	// Parse arguments
-	addr := flag.String("http", ":8000", "Serving address")
-	flag.Parse()
-
-	// Serve
-	fmt.Printf("Serving on %s\n", *addr)
-	if err := http.ListenAndServe(*addr, setup()); err != nil {
-		fmt.Printf("Error: %s\n", err)
-	}
 }
